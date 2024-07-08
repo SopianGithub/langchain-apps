dcd2d9b4e3f2b9762b45ebd79217d089eb4b2bf5
Yayan Sopian <38284814+SopianGithub@users.noreply.github.com>
2024-07-08 11:59:56 +0700

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -8,10 +8,10 @@ class Settings:
 
     GROQ_API_KEY: str = os.getenv("GROQ_API_KEY")
     
-    PINECONE_API_KEY: str = "fe42cec4-0457-4cbf-98c7-7286f92964ef"
-    PINECONE_API_INDEX: str = "apps"
+    PINECONE_API_KEY: str = ""
+    PINECONE_API_INDEX: str = ""
 
-    REDIS_CONNECT: str = "rediss://red-cpkleri0si5c73csrt90:0cxETGpmV4gYBfZDxXq6emRPUmnCIDip@singapore-redis.render.com:6379"
+    REDIS_CONNECT: str = ""
 
     LANGCHAIN_SMITH_KEY: str = os.getenv("LANGCHAIN_SMITH_KEY")
     
